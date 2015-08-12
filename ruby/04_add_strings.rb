7620118f95e04603da4a2eb2007a054476bff70b
De Wet Blomerus <dewet@blomerus.org>
2015-08-12 10:35:03 +0200

    hash & add strings exercise

---
+++ ruby/04_add_strings.rb
@@ -0,0 +1,2 @@
+#Create a program that adds two strings together and display them
+#Make up any words you want
---
+++ ruby/17_hash.rb
@@ -0,0 +1,3 @@
+# Create a hash that has everyone in the class' name as a key
+# And the neighbourhood they live in as a value
+# Use the hash a few times to get someone's neightbourhood form their name
