fe3144ecc505bc48471663ea4fa0be7f44e7c763
De Wet Blomerus <dewet@blomerus.org>
2015-09-23 09:29:03 +0200

    fizbuzz

---
+++ ruby/38_fizz.rb
@@ -0,0 +1,13 @@
+# From one to 100
+# If a number is divisible by 3, display "Fizz"
+# If a number is not divisible by 3, display the number
+# Like this
+# 1
+# 2
+# Fizz
+# 4
+# 5
+# Fizz
+# 7
+# 8
+# Fizz
---
+++ ruby/39_fizz_buzz.rb
@@ -0,0 +1,21 @@
+# From one to 100
+# If a number is divisible by 3, display "Fizz"
+# If a number is divisible by 5, display "Buzz"
+# If a number is divisible by 3 and 5 display "FizzBuzz"
+# If a number is not divisible by 3 or 5, display the number
+# Like this
+# 1
+# 2
+# Fizz
+# 4
+# Buzz
+# Fizz
+# 7
+# 8
+# Fizz
+# Buzz
+# 11
+# Fizz
+# 13
+# 14
+# FizzBuzz
